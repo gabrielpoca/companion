82d85b692975142f26853ab45ba36a80302b42dd
Gabriel Poça <gabrielpoca@gmail.com>
2019-09-24 21:32:41 +0100

    read couchdb url, username and password from env

--- src/database.js
+++ src/database.js
@@ -1,12 +1,18 @@
 const Nano = require("nano");
 const axios = require("axios");
 const assert = require("assert");
+const url = require("url");
 
 const usersDocValidation = require("./users_doc_validation");
 
 assert(process.env.COUCHDB_URL);
+assert(process.env.COUCHDB_USER);
+assert(process.env.COUCHDB_PASSWORD);
 
-const n = Nano(process.env.COUCHDB_URL);
+const couchUrl = url.parse(process.env.COUCHDB_URL);
+couchUrl.auth = `${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}`;
+
+const n = Nano(couchUrl.format());
 
 module.exports = n;
 
@@ -31,13 +37,23 @@ module.exports.setup = async () => {
     await axios.put(
       `${process.env.COUCHDB_URL}/_node/_local/_config/couch_peruser/enable`,
       '"true"',
-      {}
+      {
+        auth: {
+          username: process.env.COUCHDB_USER,
+          password: process.env.COUCHDB_PASSWORD
+        }
+      }
     );
+
+    console.log("couch_peruser enabled");
 
     const auth = await n.db.use("_users").get("_design/_auth");
     auth.validate_doc_update = usersDocValidation.toString();
     n.db.use("_users").insert(auth);
+
+    console.log("_users auth document updated");
   } catch (e) {
+    console.error(e.response);
     throw new Error("Failed to setup CouchDB");
   }
 };
